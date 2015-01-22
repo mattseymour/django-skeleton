db9b508d738e4e7eba5a4b3609bf3f6fe6e28b54
Matt Seymour <matt@mattseymour.net>
2015-01-22 16:54:45 +0000

    Adds envvars file to wsgi file

--- conf/wsgi.py
+++ conf/wsgi.py
@@ -8,7 +8,11 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.common")
+import envvars
+
+envvars_file = os.path.join('env')
+if os.path.exists(envvars_file):
+    envvars.load(envvars_file)
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
